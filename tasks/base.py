91392c18c12f1cbbe3d575d3b36d4334c0c40a4a
Eric McDonald <emcd@users.noreply.github.com>
2022-01-09 22:04:25 -0800

    Remove development dependencies on Pipenv and Tox.

--- tasks/base.py
+++ tasks/base.py
@@ -23,20 +23,22 @@
 
 import re
 
-from collections.abc import Mapping as AbstractDictionary
 from functools import partial as partial_function
-from itertools import chain
 from os import environ as psenv, pathsep
 from pathlib import Path
 from pprint import pprint
+from shlex import split as split_command
 from sys import path as python_search_paths, stderr
 
 project_path = Path( __file__ ).parent.parent
 python_search_paths.insert(
     0, str( project_path / '.local' / 'sources' / 'python3' ) )
 from our_base import (
-    ensure_python_package,
+    calculate_abi_label,
+    collapse_multilevel_dictionary,
+    indicate_python_package_dependencies,
     paths,
+    standard_execute_external,
 )
 
 
@@ -49,53 +51,64 @@ else:
     eprint = partial_function( print, file = stderr )
     epprint = partial_function( pprint, stream = stderr )
 
+# Flag if streams are attached to a TTY.
+# Can use flag to suppress ANSI SGR codes for some programs.
+on_tty = stderr.isatty( )
 
-def derive_python_venv_execution_options(
-        context, venv_path = None, variables = None
+
+def derive_venv_context_options(
+    venv_path = None, version = None, variables = None
 ):
     ''' Derives flags for Python virtual environment in execution context. '''
-    if None is venv_path:
-        version = psenv.get( 'ASDF_PYTHON_VERSION' )
-        if None is version:
-            version = next( iter( indicate_python_versions_support( ) ) )
-        venv_path = derive_python_venv_path( context, version )
+    venv_path = venv_path or derive_venv_path( version )
     return dict(
-        env = derive_python_venv_variables( venv_path, variables = variables ),
+        env = derive_venv_variables( venv_path, variables = variables ),
         replace_env = True )
 
 
-def indicate_python_versions_support( ):
-    ''' Returns supported Python versions. '''
-    regex = re.compile( r'''^python\s+(.*)$''', re.MULTILINE )
-    with paths.configuration.asdf.open( ) as file:
-        return regex.match( file.read( ) )[ 1 ].split( )
-
-
-def derive_python_venv_path( context, version, python_path = None ):
+def derive_venv_path( version = None, python_path = None ):
     ''' Derives Python virtual environment path from version handle. '''
-    python_path = python_path or detect_vmgr_python_path( context, version )
-    abi_detector_path = paths.scripts.d.python3 / 'report-abi.py'
-    abi_tag = context.run(
-        f"{python_path} {abi_detector_path} {version}",
-        hide = 'stdout' ).stdout.strip( )
-    return paths.environments / abi_tag
+    if None is python_path:
+        if version: python_path = detect_vmgr_python_path( version )
+        elif 'VIRTUAL_ENV' in psenv and 'OUR_VENV_NAME' in psenv:
+            venv_path = Path( psenv[ 'VIRTUAL_ENV' ] )
+            if venv_path.name == psenv[ 'OUR_VENV_NAME' ]: return venv_path
+    if None is python_path: python_path = detect_vmgr_python_path( )
+    abi_label = calculate_abi_label( python_path )
+    return paths.environments / abi_label
 
 
-def derive_python_venv_variables( venv_path, variables = None ):
+def derive_venv_variables( venv_path, variables = None ):
     ''' Derives environment variables from Python virtual environment path. '''
     variables = ( variables or psenv ).copy( )
     variables.pop( 'PYTHONHOME', None )
     variables[ 'PATH' ] = pathsep.join( (
         str( venv_path / 'bin' ), variables[ 'PATH' ] ) )
     variables[ 'VIRTUAL_ENV' ] = str( venv_path )
+    variables[ 'OUR_VENV_NAME' ] = venv_path.name
     return variables
 
 
-def detect_vmgr_python_path( context, version ):
-    ''' Detects Python path using version manager handle. '''
-    installation_path = Path( context.run(
-        f"asdf where python {version}", hide = 'stdout' ).stdout.strip( ) )
+def detect_vmgr_python_path( version = None ):
+    ''' Detects Python path using handle from version manager. '''
+    version = version or detect_vmgr_python_version( )
+    installation_path = Path( standard_execute_external(
+        ( *split_command( 'asdf where python' ), version ) ).stdout.strip( ) )
     return installation_path / 'bin' / 'python'
+
+
+def detect_vmgr_python_version( ):
+    ''' Detects Python handle selected by version manager. '''
+    return next( iter( indicate_python_versions_support( ) ) )
+
+
+def indicate_python_versions_support( ):
+    ''' Returns supported Python versions. '''
+    version = psenv.get( 'ASDF_PYTHON_VERSION' )
+    if None is not version: return ( version, )
+    regex = re.compile( r'''^python\s+(.*)$''', re.MULTILINE )
+    with paths.configuration.asdf.open( ) as file:
+        return regex.match( file.read( ) )[ 1 ].split( )
 
 
 def generate_pip_requirements( folio = None ):
@@ -106,19 +119,3 @@ def generate_pip_requirements( folio = None ):
         folio or indicate_python_package_dependencies( ) )
     # TODO: Handle structured entries.
     return '\n'.join( folio )
-
-
-def collapse_multilevel_dictionary( dictionary ):
-    ''' Collapses a hierarchical dictionary into a list. '''
-    return tuple( chain.from_iterable(
-        (   collapse_multilevel_dictionary( value )
-            if isinstance( value, AbstractDictionary ) else value )
-        for value in dictionary.values( ) ) )
-
-
-def indicate_python_package_dependencies( ):
-    ''' Returns dictionary of Python package dependencies. '''
-    ensure_python_package( 'tomli' )
-    from tomli import load
-    with paths.configuration.pypackages.open( 'rb' ) as file:
-        return load( file )
