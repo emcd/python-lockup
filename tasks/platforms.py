d1f6c4cf976a4cd368bb6a6bb5c9230b5b27e98f
Eric McDonald <emcd@users.noreply.github.com>
2022-04-03 17:43:48 -0700

    Invoke Tasks: Start refactor of platform management functions.

---
+++ tasks/platforms.py
@@ -0,0 +1,64 @@
+# vim: set filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+#============================================================================#
+#                                                                            #
+#  Licensed under the Apache License, Version 2.0 (the "License");           #
+#  you may not use this file except in compliance with the License.          #
+#  You may obtain a copy of the License at                                   #
+#                                                                            #
+#      http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                            #
+#  Unless required by applicable law or agreed to in writing, software       #
+#  distributed under the License is distributed on an "AS IS" BASIS,         #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#  See the License for the specific language governing permissions and       #
+#  limitations under the License.                                            #
+#                                                                            #
+#============================================================================#
+
+''' Management of development platforms. '''
+
+
+class __:
+
+    import re
+
+    from shlex import split as split_command
+    from subprocess import CalledProcessError as ProcessInvocationError
+
+    from .base import (
+        pep508_identify_python,
+        render_boxed_title,
+    )
+    from our_base import (
+        standard_execute_external,
+    )
+
+
+def freshen_python( context, original_version ):
+    ''' Updates supported Python minor version to latest patch.
+
+        This task requires Internet access and may take some time. '''
+    __.render_boxed_title(
+        'Freshen: Python Version', supplement = original_version )
+    minors_regex = __.re.compile(
+        r'''^(?P<prefix>\w+(?:\d+\.\d+)?-)?(?P<minor>\d+\.\d+)\..*$''' )
+    groups = minors_regex.match( original_version ).groupdict( )
+    minor_version = "{prefix}{minor}".format(
+        prefix = groups.get( 'prefix' ) or '',
+        minor = groups[ 'minor' ] )
+    successor_version = __.standard_execute_external(
+        ( *__.split_command( 'asdf latest python' ), minor_version )
+    ).stdout.strip( )
+    try:
+        original_identifier = __.pep508_identify_python(
+            version = original_version )
+    # Version may not be installed.
+    except __.ProcessInvocationError: original_identifier = None
+    context.run( f"asdf install python {successor_version}", pty = True )
+    # Do not erase packages fixtures for extant versions.
+    successor_identifier = __.pep508_identify_python(
+        version = successor_version )
+    if original_identifier == successor_identifier: original_identifier = None
+    return { original_version: successor_version }, original_identifier
